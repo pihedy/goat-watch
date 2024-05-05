ce15f36b147af35dc5112aee76ab19127cd40b0f
pihedy <pihedy@gmail.com>
2024-05-05 18:12:07 +0200

    Correct code.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,14 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use \Illuminate\Database\Eloquent\Factories\HasFactory;
+use \Illuminate\Foundation\Auth\User as Authenticatable;
+use \Illuminate\Notifications\Notifiable;
 
+/**
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
