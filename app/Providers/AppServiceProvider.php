581af415e19c1000cb1f6634275bf6dc7578511a
pihedy <pihedy@gmail.com>
2024-05-05 18:11:14 +0200

    Add comment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use \Illuminate\Support\ServiceProvider;
 
 /**
+ * This service provider does not have any specific services to register or bootstrap.
+ *
  * @author Pihe Edmond <pihedy@gmail.com>
  */
 class AppServiceProvider extends ServiceProvider
